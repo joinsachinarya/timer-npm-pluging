53e3f3a98e6984e01caf5b4f21a064acce2b92ca
sac6inarya <sachinkarya333@gmail.com>
2023-01-20 13:38:41 +0530

    added features

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let targetTime:number=10;
+const timer =(targetTime:number)=>{
+    let givenTime = setInterval(()=>{
+        console.log(`${targetTime} seconds remaining!`);
+        targetTime--;
+        if(targetTime===0){
+            clearInterval(givenTime);
+            console.log("Timer has ended!")
+        }
+    }, 1000)
+}
+
+timer(targetTime);
